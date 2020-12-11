0037ef838c6b5398efbcba7dd9852de95d15663a
Jo <seventheli@outlook.com>
2020-12-11 11:17:14 +0800

    for cloud test

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ result_saver = "./test_{}".format(str(time.time()))
 # the number of step it take to linearly anneal the epsilon to it min value
 annealing_end = 100
 # min level of stochastically of policy (epsilon)-greedy
-epsilon_min = 0.15
+epsilon_min = 0
 # temporary files
 temporary_model = "./{}/model_with_cnn.params".format(model_save)
 temporary_pool = "./{}/pool".format(model_save)
