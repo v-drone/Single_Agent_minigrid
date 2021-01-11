a114c124b6cd09d2626382807db5ff22a6e4aac4
Jo <seventheli@outlook.com>
2021-01-11 17:38:47 +0800

    fix bug

--- example.py
+++ example.py
@@ -54,14 +54,13 @@ for epoch in range(_epoch, num_episode):
                 f.writelines(success_text + "\n")
             if epoch % 100 == 0:
                 print(success_text)
-    #  train every 2 epoch
-    if annealing_count > replay_start_size and epoch % 2 == 0:
+            # save model and replace online model each epoch
+        if annealing_count > replay_start_size and annealing_count % update_step == 0:
+            copy_params(offline_model, online_model)
+            offline_model.save_parameters(temporary_model)
+    #  train every 4 epoch
+    if annealing_count > replay_start_size and epoch % 4 == 0:
         cost.append(algorithm.train())
-    # save model and replace online model each epoch
-    if annealing_count > replay_start_size and annealing_count % update_step == 0:
-        copy_params(offline_model, online_model)
-        offline_model.save_parameters(temporary_model)
-        print("over-write %d, last lost %d" % (_epoch, cost[-1]))
     tot_reward[int(epoch) - 1] = cum_clipped_reward
     if epoch > 50.:
         moving_average = np.mean(tot_reward[int(epoch) - 1 - 50:int(epoch) - 1])
