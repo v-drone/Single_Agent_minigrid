58c190e0c2ac673920266a5553458663f8385cfa
Jo <seventheli@outlook.com>
2021-01-14 16:19:34 +0800

    update reward function

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ import time
 action_max = 2
 # learning rate
 model_save = "./model_save/"
-lr = 0.01
+lr = 0.002
 # start play
-replay_start_size = 10000
+replay_start_size = 20000
 # update step
-update_step = 500
+update_step = 1000
 # gamma in q-loss calculation
 gamma = 0.99
 # memory pool size
-memory_length = 100000
+memory_length = 300000
 # file to save train log
 summary = "./test_{}".format(str(time.time()))
 # the number of step it take to linearly anneal the epsilon to it min value
-annealing_end = 100000
+annealing_end = 1000000
 # min level of stochastically of policy (epsilon)-greedy
 epsilon_min = 0.2
 # temporary files
