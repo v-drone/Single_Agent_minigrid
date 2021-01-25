7cf341ddf6413f08508f07c216a2735a5ec95c6c
Jo <seventheli@outlook.com>
2021-01-25 19:19:13 +0800

    add step discount

--- algorithm/reward_function.py
+++ algorithm/reward_function.py
@@ -13,10 +13,14 @@ def reward_function(old, new, basic_reward, step_count, same_position, done):
     # parameters
     b = 1
     v1 = 0.005
-    v2 = 0
+    v2 = 0.01
     # the reward from environment
     basic_reward = basic_reward * b
     # stay over
-    step_discount = - v1 * same_position
-
-    return [step_discount, basic_reward]
+    same_position = - v1 * same_position
+    # step used discount
+    if done:
+        step_discount = - v2 * step_count
+    else:
+        step_discount = 0
+    return [same_position + step_discount, basic_reward]
