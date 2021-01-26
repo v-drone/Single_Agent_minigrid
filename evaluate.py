5f8cb9ecd465807909fbd0c27a19761667721979
JO.ZHOU <chenming.zhou@earlydata.com>
2021-01-26 20:09:05 +0800

    add step count save

--- evaluate.py
+++ evaluate.py
@@ -41,12 +41,13 @@ for epoch in range(10000):
         action, by = algorithm.get_action(env.state(), eps)
         old, new, reward_get, finish, original_reward = env.step(action)
         cum_clipped_reward += original_reward
-        if finish and len(env.finish) > 50:
+        if finish:
             sr_50 = sum(env.finish[-50:]) / min(len(env.finish), 50)
             ar_50 = sum(env.total_reward[-50:]) / sum(env.total_step_count[-50:])
             sr_all = sum(env.finish) / len(env.finish)
             ar_all = sum(env.total_reward) / sum(env.total_step_count)
-            text = "success rate last 50 %f, avg return %f; success rate total %f, avg return total %f" % (sr_50, ar_50, sr_all, ar_all)
+            text = "success rate last 50 %f, avg return %f; success rate total %f, avg return total %f" % (
+            sr_50, ar_50, sr_all, ar_all)
             if epoch % 100 == 0:
                 print(text + "; %f" % eps)
         tot_reward[int(epoch) - 1] = cum_clipped_reward
@@ -63,4 +64,4 @@ print('Running after %d number of episodes' % _epoch)
 plt.xlabel("Number of episode")
 plt.ylabel("Average Reward per episode")
 plt.savefig("./default_reward_function_eval_%s.jpg" % argv[1])
-np.save("default_%s_eval.array" % argv[1], total_rew)
+np.save("default_%s_eval.array" % argv[1], env.total_step_count)
