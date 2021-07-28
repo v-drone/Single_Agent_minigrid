b8494ef18eda597239a11c97a87a60ba783c1081
seven <seventheli@outlook.com>
2021-07-29 00:04:05 +0100

    update model

--- model/simple_stack.py
+++ model/simple_stack.py
@@ -2,31 +2,33 @@ from mxnet.gluon import nn
 from mxnet import nd
 
 
+class ConvBlock(nn.Sequential):
+    def __init__(self, channels=256, kernel_size=1):
+        super().__init__()
+        self.add(nn.Conv2D(channels, kernel_size=kernel_size, strides=1,
+                           use_bias=False, layout="NCHW"))
+        self.add(nn.BatchNorm())
+        self.add(nn.Activation('softrelu'))
+
+
 class SimpleStack(nn.Block):
-    def __init__(self, agent_view, whole_map):
+    def __init__(self, agent_view, whole_map, channels=256):
         """
         MLP in mxnet with input by Lidar and Distance sensor
         """
         super(SimpleStack, self).__init__()
         with self.name_scope():
             self.view_decode = nn.Sequential()
-            self.view_decode.add(nn.Dense(1024))
-            self.view_decode.add(nn.Dense(1024))
-            self.view_decode.add(nn.Dense(128))
-            self.view_decode.add(nn.Dense(128))
+            self.view_decode.add(ConvBlock(128, 1))
+            self.view_decode.add(ConvBlock(128, 2))
+            self.view_decode.add(ConvBlock(512, 1))
             self.map_decode = nn.Sequential()
-            self.map_decode.add(nn.Dense(1024))
-            self.map_decode.add(nn.Dense(1024))
-            self.map_decode.add(nn.Dense(128))
-            self.map_decode.add(nn.Dense(128))
+            self.map_decode.add(ConvBlock(512, 1))
+            self.map_decode.add(ConvBlock(256, 3))
+            self.map_decode.add(ConvBlock(256, 3))
+            self.map_decode.add(ConvBlock(256, 3))
             self.decision_making = nn.Sequential()
-            self.decision_making.add(nn.Dense(2048))
-            self.decision_making.add(nn.Dense(2048))
-            self.decision_making.add(nn.Dense(512))
-            self.decision_making.add(nn.Dense(128))
-            self.decision_making.add(nn.Dense(64))
-            self.decision_making.add(nn.Dense(3, "relu"))
-
+            self.decision_making.add(nn.Dense(3, "tanh"))
         self.agent_view = agent_view
         self.whole_map = whole_map
 
