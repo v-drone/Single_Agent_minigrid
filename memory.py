9b95211f255f6acbea0a17a5265640c951474043
seven <seventheli@outlook.com>
2022-01-28 17:02:52 +0000

    update scripts

--- memory.py
+++ memory.py
@@ -23,20 +23,20 @@ class Memory(object):
         self.position = (self.position + 1) % self.memory_length
 
     def sample(self, batch_size,batch_state,batch_state_next,batch_reward,batch_action,batch_done, batch_battery):
-        ctx = batch_size.context
+        ctx = batch_state.context
         for i in range(batch_size):
             j = random.randint(self.frame_len-1,len(self.memory)-2)
             for jj in range(self.frame_len):
                 batch_state[i,self.frame_len-1-jj] = self.memory[j-jj].state[0].as_in_context(ctx).astype('float32')/255.
                 batch_state_next[i,self.frame_len-1-jj] = self.memory[j-jj+1].state.as_in_context(ctx)[0].astype('float32')/255.
-                if self.memory[j-jj].initial_state:
+                if self.memory[j-jj].initial:
                     for kk in range(self.frame_len-jj-1):
                         batch_state[i,self.frame_len-2-kk] =  self.memory[j-jj].state[0].as_in_context(ctx).astype('float32')/255.
                         batch_state_next[i,self.frame_len-2-kk] = self.memory[j-jj].state.as_in_context(ctx)[0].astype('float32')/255.
                     break
-            if self.memory[j].done:
+            if self.memory[j].finish:
                 batch_state_next[i,self.frame_len-1] = batch_state[i,self.frame_len-1]
             batch_reward[i] = self.memory[j].reward
             batch_action[i] = self.memory[j].action
-            batch_done[i] = self.memory[j].done
+            batch_done[i] = self.memory[j].finish
             batch_battery[i] = self.memory[j].battery
