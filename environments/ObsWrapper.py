6d66ce83c1a4cea833e0fad6561cc546bc5473a3
Jo.ZHOU <seventheli@outlook.com>
2023-09-26 08:35:08 +0800

    updating code

--- environments/ObsWrapper.py
+++ environments/ObsWrapper.py
@@ -3,8 +3,12 @@ import numpy as np
 
 
 class FullRGBImgPartialObsWrapper(RGBImgPartialObsWrapper):
+    def __init__(self, env, tile_size=8, agent_pov=False):
+        super().__init__(env, tile_size)
+        self.agent_pov = agent_pov
+
     def observation(self, obs):
-        img = self.get_frame(tile_size=self.tile_size, agent_pov=False)
+        img = self.get_frame(tile_size=self.tile_size, agent_pov=self.agent_pov)
         # Constants for the energy bar
         ENERGY_BAR_HEIGHT = self.tile_size  # Height of the energy bar in pixels
         ENERGY_BAR_COLOR_FULL = np.array([0, 255, 0])  # Green color
