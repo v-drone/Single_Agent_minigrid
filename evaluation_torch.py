824f96fa1ae14c536b050d9ca7d5cce998f49ab4
seventheli <seventheli@outlook.com>
2024-02-12 13:52:58 +0000

    update

--- evaluation_torch.py
+++ evaluation_torch.py
@@ -1,38 +1,47 @@
-from utils import create_input, translate_state
-import torch
-from PIL import Image
+import re
+import mlflow
+import numpy as np
+import onnxruntime as ort
+from IPython import display
+import matplotlib.pyplot as plt
+from utils import minigrid_env_creator
 
+# Set mlflow
+mlflow.set_tracking_uri("http://10.56.238.20:9999")
+mlflow.set_experiment(experiment_name="RouteWithTrod")
+mlflow_client = mlflow.tracking.MlflowClient()
+for_pred = "./for_pred/"
+run_id = "6ebe60216fda449c9e58d21c7392080d"
+artifacts = mlflow_client.list_artifacts(run_id)
+artifacts = [file_info for file_info in artifacts if not re.match(r'\d+.json', file_info.path)]
+for each in artifacts:
+    mlflow_client.download_artifacts(run_id=run_id, path=each.path, dst_path=for_pred)
+session = ort.InferenceSession("./for_pred/wrapped_model.onnx")
 
-def evaluate(ctx, model, env, rounds=5, print_action=False, save=None):
-    env.reset_env()
-    for epoch in range(rounds):
-        env.reset_env()
-        done = 0
-        step = 0
-        while not done:
-            step += 1
-            data = create_input([translate_state(env.map.state())])
-            data = [torch.FloatTensor(i).to(ctx) for i in data]
-            pred = model.forward(data)
-            action = int(torch.argmax(pred).cpu().numpy())
-            old, new, reward, done = env.step(action)
-            if print_action:
-                print(pred, reward, env.map.battery)
-            if save is not None:
-                img = Image.fromarray(env.map.render(), 'RGB')
-                pred = [str(x)[0:5] for x in pred.detach().numpy().tolist()[0]]
-                filename = "torch-" + str(epoch) + "-" + str(step) + "-" + str(
-                    reward) + "-" + "_".join(pred) + ".jpg"
-                img.save(save + "/" + filename)
-    return env.detect_rate
+env_config = {
+    "id": "Route",
+    "size": 12,
+    "routes": (2, 4),
+    "max_steps": 300,
+    "battery": 100,
+    "img_size": 100,
+    "tile_size": 8,
+    "num_stack": 30,
+    "render_mode": "rgb_array",
+    "agent_pov": False
+}
+env_example = minigrid_env_creator(env_config)
+actions = []
+obs, _ = env_example.reset()
 
-
-if __name__ == '__main__':
-    _agent_view = 5
-    _map_size = 20
-    _env = SimpleEnv(display=True, agent_view=_agent_view, map_size=_map_size)
-    _ctx = torch.device("cpu")
-    _model = SimpleStack()
-    _model.load_state_dict(torch.load("./model_save/Torch.params", _ctx))
-    evaluate(_ctx, _model, _env, rounds=10, print_action=True,
-             save=None)
+while True:
+    outputs = session.run(["advantage", "value", "logit"],
+                          {"obs": np.expand_dims(obs, axis=0).astype(np.float32)})
+    action = np.argmax(outputs[0])
+    actions.append(action)
+    obs, reward, terminated, truncated, info = env_example.step(action)
+    plt.imshow(env_example.render())
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    if terminated or truncated:
+        break
