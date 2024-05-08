3e9c2498d7273c3fd0b8105551e573d5c6f13592
seventheli <seventheli@outlook.com>
2024-05-08 16:30:14 +0100

    update env

--- airsim_client/airsim_server.py
+++ airsim_client/airsim_server.py
@@ -1,18 +1,29 @@
 import os
 import signal
 import subprocess
+import time
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 airsim_info = {
-    1: {"port": 41451, "path": "c:\\Users\\Administrator\\Documents\\airsimcar\\AirSimAssets.exe"},
+    0: {"port": 41451, "path": "c:\\Users\\Administrator\\Documents\\airsimcar\\AirSimAssets.exe"},
     # 1: {"port": 41451, "path": "c:\\Users\\dawei\\Desktop\\airsimcar_1\\AirSimAssets.exe"},
     # 2: {"port": 41451, "path": "c:\\Users\\dawei\\Desktop\\airsimcar_2\\AirSimAssets.exe"},
 
 }
 
 used_ports = {}
+
+
+def kill_airsim(port):
+    try:
+        os.kill(used_ports[port], signal.SIGTERM)
+        del used_ports[port]
+        return 0
+    except ProcessLookupError:
+        return 1
 
 
 def get_available_port():
@@ -25,7 +36,7 @@ def get_available_port():
 def start_airsim(path):
     if os.path.isfile(path) and os.access(path, os.X_OK):
         # batch_mode_args = ['-batchmode', '-nographics']
-        batch_mode_args = []
+        batch_mode_args = ["-nographics"]
         command = [path] + batch_mode_args
         return subprocess.Popen(command)
 
@@ -39,7 +50,8 @@ def get_client():
     info_id, port, path = get_available_port()
     if info_id is not None and port is not None and path is not None:
         process = start_airsim(path)
-        used_ports[info_id] = process.pid
+        time.sleep(5)
+        used_ports[port] = process.pid
         return jsonify({'port': port, 'client_id': info_id, 'message': 'AirSim Client started'})
     else:
         return jsonify({'message': 'No available AirSim environment'})
@@ -50,12 +62,10 @@ def cleanup_port():
     data = request.json
     if 'client_id' in data:
         client_id = data['client_id']
-        try:
-            os.kill(used_ports[client_id], signal.SIGTERM)
-            del used_ports[client_id]
-            return jsonify({'message': f'Resources cleaned up for client_id {client_id}', 'code': 0})
-        except ProcessLookupError:
-            return jsonify({'message': f'Process not found for client_id {client_id}', 'code': 1})
+        port = data['port']
+        code = kill_airsim(port)
+        return jsonify({'message': f'Resources cleaned up for client_id {client_id}', 'code': code})
+
     else:
         return jsonify({'message': f'Port not found in used ports', 'code': 2})
 
