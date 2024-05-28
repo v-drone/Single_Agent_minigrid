33a0abcb835de6e6ebb97391755eda5a237a3496
seventheli <seventheli@outlook.com>
2024-05-28 18:22:48 +0100

    update env

--- loop_check.bat
+++ loop_check.bat
@@ -9,7 +9,7 @@ REM 读取待启动的端口列表
 for /f "delims=" %%a in ('type server_ports.json ^| python -c "import sys, json; print('\n'.join(str(port) for port in json.load(sys.stdin).get('todo_ports', [])))"') do (
     REM 启动新的服务实例
     echo Starting server on port %%a...
-    start wt new-tab --title "Run AirSim with %%a" cmd /c "C:\path\to\airsim_configs\run_airsim.bat C:\path\to\airsim_configs\%%a.json"
+    start wt new-tab --title "Run AirSim with %%a" cmd /c "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.bat C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\%%a.json"
     REM 添加端口到管理服务池，确保服务启动成功后执行
     python -c "import requests; requests.post('http://127.0.0.1:7575/add', json={'port': %%a})"
 )
