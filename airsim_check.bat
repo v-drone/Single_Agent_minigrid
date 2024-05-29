9c18552a1bf74fc4ac498ec333856fbebfff77ed
seventheli <seventheli@outlook.com>
2024-05-29 20:11:34 +0100

    update env

--- airsim_check.bat
+++ airsim_check.bat
@@ -6,12 +6,10 @@ REM 运行Python脚本更新端口信息
 python airsim_check.py
 
 
-
-for /f %%port in (todo_ports.txt) do (
+for /f %%a in (todo_ports.txt) do (
     REM 启动新的服务实例
-    echo Starting server on port %%port...
-    wt new-tab --title "Run AirSim with %%port" cmd /c "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.bat C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\%%port.json"
-    REM 等待5秒后启动下一个实例
+    echo Starting server on port %%a...
+    wt new-tab --title "Run AirSim with %%a" cmd /c "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.bat C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\%%a.json"
     timeout /t 5 /nobreak
 )
 
