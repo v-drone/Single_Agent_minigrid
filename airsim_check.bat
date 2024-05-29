0c5b567a434163760225381cdcba4cb0e4b46491
seventheli <seventheli@outlook.com>
2024-05-29 20:02:57 +0100

    update env

--- airsim_check.bat
+++ airsim_check.bat
@@ -6,16 +6,10 @@ REM 运行Python脚本更新端口信息
 python airsim_check.py
 
 REM 读取待启动的端口列表
-setlocal enabledelayedexpansion
-set "cmdline="
 for /f %%a in (todo_ports.txt) do (
-    REM 构建启动新服务实例的命令
-    set "cmdline=!cmdline! new-tab --title "Run AirSim with %%a" cmd /c "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.bat C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\%%a.json" & timeout /t 5 &"
-)
-
-REM 执行命令
-if not "%cmdline%"=="" (
-    wt %cmdline%
+    REM 启动新的服务实例
+    echo Starting server on port %%a...
+    start wt new-tab --title "Run AirSim with %%a" cmd /c "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.bat C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\%%a.json"
 )
 
 echo. > todo_ports.txt
--- airsim_check.py
+++ airsim_check.py
@@ -16,16 +16,14 @@ def load_ports_from_file(filename):
 
 
 def manage_servers():
+    active_ports = requests.get("http://127.0.0.1:7575/info").json()["available"]
     backup_ports = load_ports_from_file('./backup_ports.txt')
     todo_ports = load_ports_from_file('./todo_ports.txt')
-    active_ports = requests.get("http://127.0.0.1:7575/info").json()["available"]
-    while len(active_ports) < 8:
+    if len(active_ports) < 8:
         if backup_ports:
             new_port = backup_ports.pop(0)
             todo_ports.append(new_port)
             requests.post('http://127.0.0.1:7575/add', json={'port': new_port})
-
-        active_ports = requests.get("http://127.0.0.1:7575/info").json()["available"]
 
     save_ports_to_file(backup_ports, './backup_ports.txt')
     save_ports_to_file(todo_ports, './todo_ports.txt')
