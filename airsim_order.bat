a94e56bfcd6c0092c2652c82d8aa3c865a91e48b
seventheli <seventheli@outlook.com>
2024-05-29 16:13:15 +0100

    update env

--- airsim_order.bat
+++ airsim_order.bat
@@ -1,12 +1,3 @@
 @echo off
-powershell -Command "Get-Process | Where-Object { $_.MainWindowTitle -like 'AirSimAssets' } | ForEach-Object {
-  Add-Type -Namespace Win32 -Name User32 -UsingNamespace System.Runtime.InteropServices -MemberDefinition '
-    [DllImport(\"user32.dll\")]
-    public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
-  '
-  $screen = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea
-  $width = $screen.Width / 2
-  $height = $screen.Height / 2
-  [Win32.User32]::MoveWindow($_.MainWindowHandle, 0, 0, $width, $height, $true)
-}"
+powershell -Command "$procs = Get-Process; foreach ($proc in $procs) { if ($proc.MainWindowTitle -like '*AirSimAssets*') { Add-Type -TypeDefinition '[DllImport(\"user32.dll\")] public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int Width, int Height, bool Repaint);'; $screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds; $w = $screen.Width / 2; $h = $screen.Height / 2; [Win32.User32]::MoveWindow($proc.MainWindowHandle, 0, 0, $w, $h, $true) } }"
 pause
