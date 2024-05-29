95d78e696f4123958a1b41c520b789fab766b300
seventheli <seventheli@outlook.com>
2024-05-29 20:10:33 +0100

    update env

--- airsim_check.bat
+++ airsim_check.bat
@@ -5,13 +5,18 @@ CALL C:\ProgramData\miniconda3\Scripts\activate.bat C:\Users\Administrator\.cond
 REM 运行Python脚本更新端口信息
 python airsim_check.py
 
-REM 读取待启动的端口列表
-for /f %%a in (todo_ports.txt) do (
+
+
+for /f %%port in (todo_ports.txt) do (
     REM 启动新的服务实例
-    echo Starting server on port %%a...
-    start wt new-tab --title "Run AirSim with %%a" cmd /c "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.bat C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\%%a.json"
+    echo Starting server on port %%port...
+    wt new-tab --title "Run AirSim with %%port" cmd /c "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.bat C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\%%port.json"
+    REM 等待5秒后启动下一个实例
+    timeout /t 5 /nobreak
 )
 
+:check
+REM 清空todo_ports.txt
 echo. > todo_ports.txt
 
 REM 提供退出选项
