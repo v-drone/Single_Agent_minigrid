098c2d6675aed74d047a3cfde85c3526847eec12
seventheli <seventheli@outlook.com>
2024-05-29 19:53:27 +0100

    update env

--- airsim_check.py
+++ airsim_check.py
@@ -1,15 +1,4 @@
 import requests
-import logging
-import json
-
-
-def check_server(port):
-    try:
-        response = requests.get(f"http://127.0.0.1:{port}/ping", timeout=5)
-        return response.status_code == 200
-    except Exception as e:
-        logging.error(f"Ping failed: {str(e)}")
-        return False
 
 
 def save_ports_to_file(ports, filename):
@@ -30,7 +19,7 @@ def manage_servers():
     active_ports = requests.get("http://127.0.0.1:7575/info").json()["available"]
     backup_ports = load_ports_from_file('./backup_ports.txt')
     todo_ports = load_ports_from_file('./todo_ports.txt')
-    if len(active_ports) <= 9:
+    if len(active_ports) < 8:
         if backup_ports:
             new_port = backup_ports.pop(0)
             todo_ports.append(new_port)
