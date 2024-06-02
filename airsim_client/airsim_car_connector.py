e71c172f644d3c38e0ca4006cf08a463e0230c40
seventheli <seventheli@outlook.com>
2024-06-02 16:56:09 +0100

    update env

--- airsim_client/airsim_car_connector.py
+++ airsim_client/airsim_car_connector.py
@@ -27,7 +27,7 @@ class CarConnector(object):
 
     def do_action(self, action):
         if action == 0:
-            self.car_controls.throttle = 0.05
+            self.car_controls.throttle = 0.1
             self.car.setCarControls(self.car_controls)
             time.sleep(0.2)
         elif action == 1:
@@ -36,22 +36,22 @@ class CarConnector(object):
             self.car.setCarControls(self.car_controls)
             time.sleep(0.2)
         elif action == 2:
-            self.car_controls.brake = 0
+            self.car_controls.brake = -0.01
             self.car_controls.steering = 0.5
             self.car.setCarControls(self.car_controls)
             time.sleep(0.2)
         elif action == 3:
-            self.car_controls.brake = 0
+            self.car_controls.brake = -0.01
             self.car_controls.steering = 1
             self.car.setCarControls(self.car_controls)
             time.sleep(0.2)
         elif action == 4:
-            self.car_controls.brake = 0
+            self.car_controls.brake = -0.01
             self.car_controls.steering = -0.5
             self.car.setCarControls(self.car_controls)
             time.sleep(0.2)
         elif action == 5:
-            self.car_controls.brake = 0
+            self.car_controls.brake = -0.01
             self.car_controls.steering = -1
             self.car.setCarControls(self.car_controls)
             time.sleep(0.2)
