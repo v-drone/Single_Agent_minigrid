eb3bf5769d2dd8f93698603676dd31135460bb4f
seventheli <seventheli@outlook.com>
2024-06-02 16:54:35 +0100

    update env

--- airsim_client/airsim_car_connector.py
+++ airsim_client/airsim_car_connector.py
@@ -29,39 +29,39 @@ class CarConnector(object):
         if action == 0:
             self.car_controls.throttle = 0.05
             self.car.setCarControls(self.car_controls)
-            time.sleep(0.3)
+            time.sleep(0.2)
         elif action == 1:
             self.car_controls.brake = -0.2
             self.car_controls.throttle = 0
             self.car.setCarControls(self.car_controls)
-            time.sleep(0.3)
+            time.sleep(0.2)
         elif action == 2:
             self.car_controls.brake = 0
             self.car_controls.steering = 0.5
             self.car.setCarControls(self.car_controls)
-            time.sleep(0.3)
+            time.sleep(0.2)
         elif action == 3:
             self.car_controls.brake = 0
             self.car_controls.steering = 1
             self.car.setCarControls(self.car_controls)
-            time.sleep(0.3)
+            time.sleep(0.2)
         elif action == 4:
             self.car_controls.brake = 0
             self.car_controls.steering = -0.5
             self.car.setCarControls(self.car_controls)
-            time.sleep(0.3)
+            time.sleep(0.2)
         elif action == 5:
             self.car_controls.brake = 0
             self.car_controls.steering = -1
             self.car.setCarControls(self.car_controls)
-            time.sleep(0.3)
+            time.sleep(0.2)
         elif action == 6:
             self.car_controls.throttle = 0
             self.car_controls.steering = 0
             self.car.setCarControls(self.car_controls)
-            time.sleep(0.3)
+            time.sleep(0.2)
         else:
-            time.sleep(0.3)
+            time.sleep(0.2)
 
         return self.get_info()
 
