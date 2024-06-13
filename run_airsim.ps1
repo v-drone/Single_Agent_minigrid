0450c8599f7f227deb7e90899925421465ced288
seventheli <seventheli@outlook.com>
2024-06-13 16:03:58 +0100

    update code

---
+++ run_airsim.ps1
@@ -0,0 +1,12 @@
+# PowerShell script equivalent
+
+# Navigate to the specified directory
+Set-Location -Path "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid"
+
+# Run the Python script with the provided command line argument
+python .\airsim_client\airsim_runner.py -f $args[0]
+
+# Output completion message with the configuration file used
+Write-Host "`nTask completed with config: $($args[0])"
+Write-Host "Press any key to close this window..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
--- start_all_air.ps1
+++ start_all_air.ps1
@@ -1,47 +1,36 @@
-# 禁用回显
 $ErrorActionPreference = 'SilentlyContinue'
 
-# 激活 Python 环境
-& C:\ProgramData\miniconda3\Scripts\Activate.ps1 C:\Users\Administrator\.conda\envs\client
-
-# 创建并覆盖 backup_ports.txt 文件
 Write-Host "Writing to backup_ports.txt"
 5000..5400 | ForEach-Object { $_ } | Set-Content "backup_ports.txt"
 Write-Host "Ports backup completed."
 
-# 等待3秒
 Start-Sleep -Seconds 3
 
-# 主循环
 do
 {
-    # 运行Python脚本更新端口信息
     python airsim_check.py
 
-    # 处理todo_ports.txt文件中的端口
     Get-Content todo_ports.txt | ForEach-Object {
         $port = $_
         Write-Host "Starting Docker container for port $port..."
         docker start "$port"
 
         Write-Host "Starting server on port $port..."
-        Start-Process -FilePath "cmd" -ArgumentList "/c", "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.bat C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\$port.json" -NoNewWindow
+        # Call the PowerShell script instead of cmd batch file
+        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.ps1", "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\$port.json" -NoNewWindow
 
         Start-Sleep -Seconds 5
     }
 
-    # 处理died_ports.txt文件中的端口
     Get-Content died_ports.txt | ForEach-Object {
         $port = $_
         Write-Host "Stopping and removing Docker container for port $port..."
         docker stop "$port"
     }
 
-    # 清空todo_ports.txt和died_ports.txt
     Clear-Content todo_ports.txt
     Clear-Content died_ports.txt
 
-    # 等待10秒或直到按键被按下
     Write-Host "Waiting for 10 seconds. Press any key to exit."
     $startTime = Get-Date
     while ((New-TimeSpan -Start $startTime -End (Get-Date)).TotalSeconds -lt 10 -and -not [Console]::KeyAvailable) {
