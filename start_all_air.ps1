affad03bbac2955257052648f37f487ffcd78221
seventheli <seventheli@outlook.com>
2024-06-13 17:48:12 +0100

    update code

--- start_all_air.ps1
+++ start_all_air.ps1
@@ -16,8 +16,8 @@ do
         docker start "$port"
 
         Write-Host "Starting server on port $port..."
-        # Call the PowerShell script instead of cmd batch file
-        Start-Process -FilePath "powershell.exe" -ArgumentList '-NoProfile', '-ExecutionPolicy', 'Bypass', '-File', 'C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.ps1', 'C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\$port.json', 'C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\Logs\$port.log' -NoNewWindow
+        # Correct usage in PowerShell
+        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\run_airsim.ps1", "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\airsim_configs\$port.json", "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid\Logs\$port.log" -NoNewWindow
 
         Start-Sleep -Seconds 5
     }
