f1f956ccfe1b1ca8a09874ef14a4a1893af6bc58
seventheli <seventheli@outlook.com>
2024-06-13 17:08:17 +0100

    update code

--- run_airsim.ps1
+++ run_airsim.ps1
@@ -4,7 +4,7 @@
 Set-Location -Path "C:\Users\Administrator\Documents\UAV\Single_Agent_minigrid"
 
 # Run the Python script with the provided command line argument
-python .\airsim_client\airsim_runner.py -f $args[0]
+python .\airsim_client\airsim_runner.py -f $args[0] -l $args[1]
 
 # Output completion message with the configuration file used
 Write-Host "`nTask completed with config: $($args[0])"
