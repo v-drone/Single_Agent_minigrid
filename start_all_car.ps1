d76b06884625cbb734ee80660ab156cfcd339d39
seventheli <seventheli@outlook.com>
2024-12-10 19:31:10 +0000

    update env

--- start_all_car.ps1
+++ start_all_car.ps1
@@ -1,5 +1,5 @@
 Write-Host "Writing to backup_ports.txt"
-5050..5100 | ForEach-Object { $_ } | Set-Content "backup_ports.txt"
+5030..5100 | ForEach-Object { $_ } | Set-Content "backup_ports.txt"
 Clear-Content todo_ports.txt
 Clear-Content died_ports.txt
 
